b5b6fd0c51cd3e0b1d98df6e09ef16aa1525b3bc
Teashick <dhxogus005@gmail.com>
2026-08-19 10:25:01 +0900

    d

--- 00_io/ex1.py
+++ 00_io/ex1.py
@@ -1,7 +1,26 @@
 #입출력 처리
-
 #1개 입력
-input()
+#input()
+#a = input()
+#print(a)
+#print(type(a))
+
+#정수 변환
 a = input()
-print(a)
+a = int(a)
 print(type(a))
+
+#한번에 쓰자
+a = int(input())
+print(type(a))
+
+#실수 입력
+b = float(input())
+print(b,type(b))
+
+#정수 2개 입력
+#100
+#200
+a = int(input())
+b = int(input())
+print(a, b)
